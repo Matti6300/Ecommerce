62241b9447b91845df3bd0c59962c17066470c17
bharath8858 <105233330+bharath8858@users.noreply.github.com>
2024-06-06 11:21:55 +0530

    hi

--- src/test/java/Com/nopecommerce/Registration/RegistaringAnUSerTest.java
+++ src/test/java/Com/nopecommerce/Registration/RegistaringAnUSerTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 import com.nopcommerce.ObjectRepo.ComputerPage;
 import com.nopecommerce.BaseClass.BaseClassTest;
 
-public class RegistaringAnUSer extends BaseClassTest {
+public class RegistaringAnUSerTest extends BaseClassTest {
 	@Test
 	public void regusteringAuser() throws InterruptedException {
 		System.out.println("hi");
